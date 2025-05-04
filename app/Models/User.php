9fd7c1728acf10a29bcdcae15c6c61098e47087e
TrejoJulian <trejojulian998@gmail.com>
2025-05-04 19:19:30 -0300

    Feature favourite
    - New table to handle products favourited by users.
    - New endpoint to favourite products as users and to remove the favourite.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -45,5 +45,9 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function favouriteProducts(): BelongsToMany {
+        return $this->belongsToMany(Product::class)->withTimestamps();
     }
 }
